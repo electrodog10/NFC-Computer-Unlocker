4d8ec1b7576e45ff8e61f3842d44c63eadde9d12
electrodog10 <lewis@lewisproductions.com>
2015-04-25 08:30:51 -0500

    Cleaned Up Code

--- encryption:final.ino
+++ encryption:final.ino
@@ -19,7 +19,6 @@ void setup() {
     while (1); // halt
   }
 
-  //Got ok data, print it out!
   Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
   Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC);
   Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
